1582fe163dfc621676fb0d8da71c8ea79d0d88cf
ginorojo <ginorojoj@gmail.com>
2025-05-29 15:10:55 -0400

    otro cambios

--- src/hooks/useStays.jsx
+++ src/hooks/useStays.jsx
@@ -7,7 +7,7 @@ export function useStays() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get('https://ginorojo.github.io/miniproyecto-react-stays/data/stays.json')
+    axios.get('stays.json')
       .then(response => {
         setStays(response.data);
         setTimeout(() => {
